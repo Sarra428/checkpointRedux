e2c0d5989dd24280d1c38d3ca0f3309d31b62334
sarra <sarrah.aouina@gmail.com>
2022-01-10 14:41:25 +0100

    tasks redux

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import AddTask from './Components/AddTask';
+import TaskList from './Components/TaskList';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <h1>checkpoint redux</h1>
+
+    <AddTask/> <br/>
+    <TaskList/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Redux/Types.js
@@ -0,0 +1,9 @@
+
+
+export const ADDTODO = "ADDTODO"
+
+export const DONETODO = "DONETODO"
+
+export const DELETETODO = "DELETETODO"
+
+export const EDITTODO = "EDITTODO"

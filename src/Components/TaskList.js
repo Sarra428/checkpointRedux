ca52317b81ae456cf4e448cac29d9649fb1bea2d
sarra <sarrah.aouina@gmail.com>
2022-01-10 14:46:37 +0100

    commit

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -18,8 +18,8 @@ function TaskList() {
 
 
   <Button variant="outline-success"   onClick={()=> setFilter(false)}              >Show All Tasks</Button>{' '}
-  <Button variant="outline-warning" onClick={()=> {setFilter(true); setDone(false)}} >Show All Done</Button>{' '}
-  <Button variant="outline-danger"  onClick={()=> {setFilter(true); setDone(true)}}  >Show All Not Done</Button>{' '}
+  <Button variant="outline-warning" onClick={()=> {setFilter(true); setDone(false)}} >Show All Not Done</Button>{' '}
+  <Button variant="outline-danger"  onClick={()=> {setFilter(true); setDone(true)}}  >Show All Done</Button>{' '}
  
      {!filter  ? todos.map((el) => <Task key={el.id} el={el} />)   : done
             ? todos.filter((el) => el.done).map((el) => <Task key={el.id} el={el} />)      : todos.filter((el) => !el.done).map((el) => <Task key={el.id} el={el} />)}
